2be86d272c46bb0fa99374684a262f8bc76fef7f
peter <pngugi.wainaina@gmail.com>
2023-12-19 00:50:55 +0300

    code updated

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+const baseURL = "http://localhost:6001/plants";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(baseURL)
+      .then((resp) => resp.json())
+      .then((allPlants) => setPlants(allPlants));
+  }, []);
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
